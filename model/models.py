78c25ff8041f1e0a8922c9dbe26dceb90e4a59d2
Prasan Yapa <toprasanyapa@gmail.com>
2020-01-16 13:45:37 +0530

    Code Refactoring.

--- model/models.py
+++ model/models.py
@@ -7,13 +7,6 @@ from keras.models import Model, model_from_json
 from model.layers import CRF
 
 
-def save_model(model, weights_file, params_file):
-    with open(params_file, 'w') as f:
-        params = model.to_json()
-        json.dump(json.loads(params), f, sort_keys=True, indent=4)
-        model.save_weights(weights_file)
-
-
 def load_model(weights_file, params_file):
     with open(params_file) as f:
         model = model_from_json(f.read(), custom_objects={'CRF': CRF})
@@ -90,66 +83,3 @@ class BiLSTM(object):
         model = Model(inputs=inputs, outputs=pred)
 
         return model, loss
-
-
-class ELModel(object):
-    def __init__(self,
-                 num_labels,
-                 word_vocab_size,
-                 char_vocab_size=None,
-                 word_embedding_dim=100,
-                 char_embedding_dim=25,
-                 word_lstm_size=100,
-                 char_lstm_size=25,
-                 fc_dim=100,
-                 dropout=0.5,
-                 embeddings=None):
-        self._char_embedding_dim = char_embedding_dim
-        self._word_embedding_dim = word_embedding_dim
-        self._char_lstm_size = char_lstm_size
-        self._word_lstm_size = word_lstm_size
-        self._char_vocab_size = char_vocab_size
-        self._word_vocab_size = word_vocab_size
-        self._fc_dim = fc_dim
-        self._dropout = dropout
-        self._embeddings = embeddings
-        self._num_labels = num_labels
-
-    def build(self):
-        # build word embedding
-        word_ids = Input(batch_shape=(None, None), dtype='int32', name='word_input')
-        if self._embeddings is None:
-            word_embeddings = Embedding(input_dim=self._word_vocab_size,
-                                        output_dim=self._word_embedding_dim,
-                                        mask_zero=True,
-                                        name='word_embedding')(word_ids)
-        else:
-            word_embeddings = Embedding(input_dim=self._embeddings.shape[0],
-                                        output_dim=self._embeddings.shape[1],
-                                        mask_zero=True,
-                                        weights=[self._embeddings],
-                                        name='word_embedding')(word_ids)
-
-        char_ids = Input(batch_shape=(None, None, None), dtype='int32', name='char_input')
-        char_embeddings = Embedding(input_dim=self._char_vocab_size,
-                                    output_dim=self._char_embedding_dim,
-                                    mask_zero=True,
-                                    name='char_embedding')(char_ids)
-        char_embeddings = TimeDistributed(Bidirectional(LSTM(self._char_lstm_size)))(char_embeddings)
-
-        elmo_embeddings = Input(shape=(None, 1024), dtype='float32')
-
-        word_embeddings = Concatenate()([word_embeddings, char_embeddings, elmo_embeddings])
-
-        word_embeddings = Dropout(self._dropout)(word_embeddings)
-        z = Bidirectional(LSTM(units=self._word_lstm_size, return_sequences=True))(word_embeddings)
-        z = Dense(self._fc_dim, activation='tanh')(z)
-
-        crf = CRF(self._num_labels, sparse_target=False)
-        loss = crf.loss_function
-        pred = crf(z)
-
-        model = Model(inputs=[word_ids, char_ids, elmo_embeddings], outputs=pred)
-
-        return model, loss
-
